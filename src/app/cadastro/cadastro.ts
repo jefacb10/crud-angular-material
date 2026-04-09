f0b990a98d2663966eec8b34e265947d8e4655fb
jeferson.barros <jeferson.barros@bancobmg.com.br>
2026-04-09 20:28:50 -0300

    Implementação da Tela de Cadastro

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  standalone: true,
+  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './cadastro.html',
-  styleUrl: './cadastro.scss',
+  styleUrls: ['./cadastro.scss'],
 })
+
 export class CadastroComponent {
 
 }
